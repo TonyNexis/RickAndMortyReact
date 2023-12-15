4a591eccf77708d01d4f0c7a1d373293be424e2b
Anton Tymchyk <tony.zenekis@gmail.com>
2023-12-15 15:05:02 +0200

    some form changes

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -17,7 +17,9 @@ const RegForm = () => {
         password: '',
     })
 
-    const closeModalReg = () => {
+    const closeModalReg = (e) => {
+        e.preventDefault();
+
         dispatch(modalDisplayOff())
         setFormValues({
             nickname: '',
@@ -62,28 +64,30 @@ const RegForm = () => {
         if (display) {
             const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
             document.body.style.overflow = 'hidden';
-            // document.body.style.overflowY = 'scroll';
             document.body.style.paddingRight = `${scrollbarWidth}px`;
-            // document.body.style.paddingRight = '17px';
             document.querySelector('.menuPanel_navigation_panel__9TC-N ').style.marginRight = `${scrollbarWidth+ 15}px`;
-            // const regFormElement = document.querySelector('.menuPanel_navigation_panel__9TC-N ');
-            // if (regFormElement) {
-            //     regFormElement.style.marginRight = '32px';
-            // }
+            document.querySelector('.form_reg_form__4s1Kl').style.marginRight = ``;
         } else {
-          document.body.style.overflow = 'auto';
-          document.body.style.paddingRight = '';
-          document.querySelector('.menuPanel_navigation_panel__9TC-N ').style.marginRight = '15px';
+            document.body.style.overflow = 'auto';
+            document.body.style.paddingRight = '';
+            document.querySelector('.menuPanel_navigation_panel__9TC-N ').style.marginRight = '15px';
+
+            const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+            
+            document.querySelector('.form_reg_form__4s1Kl').style.marginRight = `-${scrollbarWidth}px`;
         }
       }, [display]);
 
     return (
         <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={display }
+        open={display}
         onClick={closeModalReg}>
             <form className={styles.reg_form} onClick={handleFormClick} action="">
             <h1>Create an account</h1>
+            <button className={styles.close_btn} onClick={closeModalReg}><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
+    <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path>
+</svg></button>
             <TextField
                 className={styles.input_field}
                 size="small"

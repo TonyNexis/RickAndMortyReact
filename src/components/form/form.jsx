bd2f40da5403cb40c6fc6803353e2f83bf3841b7
Anton <tony.zenekis@gmail.com>
2023-12-16 00:05:17 +0200

    some form passwod ui changes

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,14 +2,17 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { modalDisplayOff, modalDisplay } from '../../redux/modalSlice';
-import { Button } from '@mui/material';
-import { TextField, Backdrop } from '@mui/material';
+import { Button, TextField, Backdrop, InputAdornment, FormControl, InputLabel, OutlinedInput } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 import styles from './form.module.scss';
 
 
 const RegForm = () => {
     const dispatch = useDispatch();
+    const [showPassword, setShowPassword] = React.useState(false);
 
     const [formValues, setFormValues] = useState({
         nickname: '',
@@ -26,7 +29,10 @@ const RegForm = () => {
             login: '',
             password: '',
         })
+        setShowPassword(false);
     }
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     useEffect(() => {
         const closeEscape = e => {
@@ -85,9 +91,9 @@ const RegForm = () => {
         onClick={closeModalReg}>
             <form className={styles.reg_form} onClick={handleFormClick} action="">
             <h1>Create an account</h1>
-            <button className={styles.close_btn} onClick={closeModalReg}><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
-    <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path>
-</svg></button>
+            <button className={styles.close_btn} onClick={closeModalReg}>
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30"><path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path></svg>
+            </button>
             <TextField
                 className={styles.input_field}
                 size="small"
@@ -108,7 +114,7 @@ const RegForm = () => {
                 value={formValues.login}
                 onChange={handleInputChange}
             />
-            <TextField
+            {/* <TextField
                 className={styles.input_field}
                 size="small"
                 id="password"
@@ -117,7 +123,29 @@ const RegForm = () => {
                 autoComplete="current-password"
                 value={formValues.password}
                 onChange={handleInputChange}
-            />
+            /> */}
+
+            <FormControl size="small" className={styles.input_field} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton 
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                        >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                    label="Password"
+                    value={formValues.password}
+                    onChange={handleInputChange}
+                />
+            </FormControl>
             <Button className={styles.reg_btn} variant="contained">Register</Button>
             </form>
         </Backdrop>

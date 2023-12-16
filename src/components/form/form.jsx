558c37b2cb9e731dcfe5773a65a4c625dcf07bc3
Anton <tony.zenekis@gmail.com>
2023-12-16 23:22:13 +0200

    added react hook form func

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,5 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { modalDisplayOff, modalDisplay } from '../../redux/modalSlice';
 import { Button, TextField, Backdrop, InputAdornment, FormControl, InputLabel, OutlinedInput } from '@mui/material';
@@ -11,6 +12,15 @@ import styles from './form.module.scss';
 
 
 const RegForm = () => {
+    const {
+        register,
+        formState: { errors, isValid }, 
+        handleSubmit,
+        reset,
+    } = useForm({
+        mode: 'onBlur',
+    });
+
     const dispatch = useDispatch();
     const [showPassword, setShowPassword] = React.useState(false);
 
@@ -30,6 +40,7 @@ const RegForm = () => {
             password: '',
         })
         setShowPassword(false);
+        reset();
     }
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -42,7 +53,8 @@ const RegForm = () => {
                     nickname: '',
                     login: '',
                     password: '',
-                })
+                });
+                reset();
             }
         }
 
@@ -66,6 +78,10 @@ const RegForm = () => {
         }))
     }
 
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }
+
     useEffect(() => {
         if (display) {
             const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
@@ -82,14 +98,14 @@ const RegForm = () => {
             
             document.querySelector('.form_reg_form__4s1Kl').style.marginRight = `-${scrollbarWidth}px`;
         }
-      }, [display]);
+    }, [display]);
 
     return (
         <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={display}
         onClick={closeModalReg}>
-            <form className={styles.reg_form} onClick={handleFormClick} action="">
+            <form className={styles.reg_form} onClick={handleFormClick} onSubmit={handleSubmit(onSubmit)} action="">
             <h1>Create an account</h1>
             <button className={styles.close_btn} onClick={closeModalReg}>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30"><path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z"></path></svg>
@@ -101,8 +117,19 @@ const RegForm = () => {
                 label="Nickname"
                 type="text"
                 autoComplete="current-password"
-                value={formValues.nickname}
-                onChange={handleInputChange}
+                // value={formValues.nickname}
+                // onChange={handleInputChange}
+                {...register('nickname', {
+                    required: 'All fields are required.',
+                    minLength: {
+                        value: 6,
+                        message: 'Minimum Nickname length is 6 characters.'
+                    },
+                    maxLength: {
+                        value: 14,
+                        message: 'Maximum Nickname length is 14 characters.'
+                    },
+                })}
             />
             <TextField
                 className={styles.input_field}
@@ -111,8 +138,19 @@ const RegForm = () => {
                 label="Login"
                 type="text"
                 autoComplete="current-password"
-                value={formValues.login}
-                onChange={handleInputChange}
+                // value={formValues.login}
+                // onChange={handleInputChange}
+                {...register('login', {
+                    required: 'All fields are required.',
+                        minLength: {
+                        value: 6,
+                        message: 'Minimum Login length is 6 characters.'
+                    },
+                    maxLength: {
+                        value: 14,
+                        message: 'Maximum Login length is 14 characters.'
+                    },
+                })}
             />
             {/* <TextField
                 className={styles.input_field}
@@ -142,11 +180,26 @@ const RegForm = () => {
                     </InputAdornment>
                     }
                     label="Password"
-                    value={formValues.password}
-                    onChange={handleInputChange}
+                    // value={formValues.password}
+                    // onChange={handleInputChange}
+                    {...register('password', {
+                        required: 'All fields are required.',
+                        minLength: {
+                            value: 6,
+                            message: 'Minimum Password length is 6 characters.'
+                        },
+                        maxLength: {
+                            value: 14,
+                            message: 'Maximum Password length is 14 characters.'
+                        },
+                    })}
                 />
             </FormControl>
-            <Button className={styles.reg_btn} variant="contained">Register</Button>
+            <div className={styles.error_message}>
+                {(errors?.nickname || errors?.login || errors?.password) && (
+                <p>{(errors?.nickname || errors?.login || errors?.password)?.message || 'Error!'}</p>)}
+            </div>
+            <Button type='submit' className={styles.reg_btn} variant="contained" disabled={!isValid}>Register</Button>
             </form>
         </Backdrop>
     )

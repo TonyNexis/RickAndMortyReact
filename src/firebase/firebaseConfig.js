85e6eab46b03f94e4ed0c0706d172703e83df94a
Anton <tony.zenekis@gmail.com>
2024-01-17 00:46:50 +0200

    some firebase changes

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,20 +1,20 @@
-import { getAnalytics } from "firebase/analytics"
+// import { getAnalytics } from "firebase/analytics"
 import { initializeApp } from "firebase/app"
 import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfER8DlsuXNcK5sGP6U1r20NV1-sALZXY",
-  authDomain: "rick-and-morty-ef8c2.firebaseapp.com",
-  projectId: "rick-and-morty-ef8c2",
-  storageBucket: "rick-and-morty-ef8c2.appspot.com",
-  messagingSenderId: "839564930426",
-  appId: "1:839564930426:web:5b95a6e0e1da735fd2bac3",
-  measurementId: "G-0CDRE58BL6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
